1dacf5f96cae8c4692933edc7912358238c05b23
shayda-banihashemi <shayda.dev@icloud.com>
2024-10-20 11:44:12 -0700

    Created separate articles_app - imported weeks, seasons, and articles in app.py.

---
+++ src/nflai/articles_app.py
@@ -0,0 +1,10 @@
+def articles_gather_data():
+    article_docs = []
+    with open("/Users/shaydabanihashemi/ws/nflai/data/general/nfl_article.txt") as f:
+        for lines in f:
+            line = lines.rstrip()
+            article_docs.append(line)
+    return article_docs
+
+if __name__ == "__main__":
+    articles_gather_data()
