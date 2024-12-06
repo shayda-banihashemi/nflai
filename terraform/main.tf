6a246e788d30ea0acfbb34c8f459e55c531b7c59
shayda-banihashemi <shayda.dev@icloud.com>
2024-12-05 18:42:49 -0700

    updated security group for allow_ssh

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ resource "aws_key_pair" "deployer" {
 }
 
 resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
+  name        = "allow_ssh_1"
   description = "Allow SSH inbound traffic"
 
   ingress {
